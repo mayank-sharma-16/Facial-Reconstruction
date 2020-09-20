c94502bb27724d5f4e0c2788f615ab91ffa5597e
manksharma <2mayanksharma2@gmail.com>
2020-09-20 10:19:34 -0700

    massive refactoring and comments, added overlay of fixed image on old

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ def SetupWindows(settings: dict) -> cv2.VideoCapture:
           types as values depending on the setting.
           
     Returns:
-        input_capture: the VideoCapture object used to get facial images.
+        input_capture: the VideoCapture object that feeds into the initial Windows.
 
     Raises: 
         CaptureOpenError: Unable to open default video capture channel.
@@ -68,13 +68,14 @@ def SetupWindows(settings: dict) -> cv2.VideoCapture:
     if not input_capture.isOpened():
         raise CaptureOpenError
 
-    cv2.namedWindow("Input Face")
+    # Creates essential windows
+    cv2.namedWindow("Artificial Errors")
     cv2.namedWindow("Reconstructed Face")
 
     # Creates windows for intermediary transformations if debug mode is on.
     if settings["debug"]:
-        cv2.namedWindow("Artificial Errors")
-        cv2.namedWindow("Reconstructed Frame - Small")
+        cv2.namedWindow("Input Face")
+        cv2.namedWindow("Reconstructed Face - Small")
 
     return input_capture
 
@@ -86,16 +87,16 @@ def MakeErrors(frame: np.ndarray, pattern_number: int = 0) -> np.ndarray:
 
     Args:
         frame: a 2-dimensional np.ndarray with the black-and-white values of
-          each pixel as an integer.
+          each pixel as a double.
         pattern_number: integer that correlates to a particular pattern.
 
     Returns:
         A new 2-dimensional np.ndarray with black-and-white values of each
-          pixel as an integer.
+          pixel as a double.
     """
 
     # TODO: add more patterns
-    # TODO: make a better process
+    # TODO: refactor to account for more patterns
 
     broken_frame = np.copy(frame)
     
@@ -105,6 +106,36 @@ def MakeErrors(frame: np.ndarray, pattern_number: int = 0) -> np.ndarray:
                 broken_frame[int(len(broken_frame)/4)+j][i] = 0
     
     return broken_frame
+
+def OverlayFrame(top_frame: np.ndarray, broken_frame: np.ndarray, pattern_number=0):
+    """Fills in the broken bottom frame with parts of the top frame as per the specified
+    pattern number.
+
+    Runs on the assumption that the bottom frame has 'corrupted' blacked out portions that
+    the top frame can fill in, using the same pattern used to corrupt it.
+
+    Args:
+        top_frame: a 2-dimensional np.ndarray with the black and white values of each pixel
+          as a double.
+        bottom_frame: a 2-dimensional np.ndarray with the black and white values of each pixel
+          as a double.
+        pattern_number: integer that correlates to a particular pattern.
+
+    Returns:
+        A new 2-dimensional np.ndarray with black-and-white values of each
+          pixel as a double.
+
+    """
+    
+    broken_frame = np.copy(broken_frame)
+
+    if pattern_number == 0:
+        for i in range(len(broken_frame)):
+            for j in range(20):
+                broken_frame[int(len(broken_frame)/4)+j][i] = int(top_frame[int(len(top_frame)/4)+j][i]*254)
+
+    return broken_frame
+    
 
 if __name__ == "__main__":
 
@@ -122,19 +153,24 @@ if __name__ == "__main__":
         grey_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         # Introduce artificial errors.
-        broken_grey_frame = MakeErrors(grey_frame, 0)
+        broken_grey_frame = MakeErrors(cv2.resize(grey_frame, (256, 256)), 0)
 
         # Resize the frame to match the model dimensions.
         small_bgf = cv2.resize(broken_grey_frame, (64, 64))
-        fixed_frame = cv2.resize(RBM.get_RBM(small_bgf), (256, 256))
+        generated_frame = cv2.resize(RBM.get_RBM(small_bgf), (256, 256))
+        fixed_frame = OverlayFrame(generated_frame, broken_grey_frame, 0)
+
+        if settings["debug"] == True:
+            cv2.imshow("Input Face", grey_frame)
+            cv2.imshow("Reconstructed Face - Small", generated_frame)
 
         # Update all windows.
-        cv2.imshow("Input Face", grey_frame)
+        cv2.imshow("Artificial Errors", broken_grey_frame)
         cv2.imshow("Reconstructed Face", fixed_frame)
 
         # Gets next frame.
         rval, frame = input_capture.read()
-        key = cv2.waitKey(20)
+        key = cv2.waitKey(1)
 
         # Releases capture on ESC
         if key == 27:
